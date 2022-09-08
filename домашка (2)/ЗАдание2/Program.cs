95841f25e6debf8ac86beb4b0eb9195deb9ca6e4
Nameuser9 <ivamand2002@gmail.com>
2022-09-08 15:41:16 +0700

    Домашка

---
+++ домашка (2)/ЗАдание2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёх значное число, или же двухзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int n3 = (a % 100) % 10;
+if (n3 < 1)
+{
+    Console.WriteLine("ТРЕТЬЕГО ЧИСЛА НЕТ");
+}
+else 
+{
+    Console.Write("Третье число - ");
+    Console.Write(n3);
+}
+
---
+++ домашка (2)/ЗАдание3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число, обозначающее день недели от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else 
+{
+    Console.WriteLine("Выходной день");
+}
