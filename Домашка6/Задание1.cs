5709edbd2abc3a11f80bd279788b4523566ef133
Nameuser9 <ivamand2002@gmail.com>
2022-09-23 01:13:32 +0700

    Сделал 6 домашку( во второй задаче двумерную матрицу реализовать пока не получилось(((
    во второй задаче двумерную матрицу реализовать пока не получилось(((
    потом попробую ещё...

---
+++ Домашка6/Задание1.cs
@@ -0,0 +1,36 @@
+﻿void Fillarray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] =Convert.ToInt32(Console.ReadLine());                    //new Random().Next(-1000, 1001);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    int chislb0 = 0;
+    while (position < count)
+    {
+        
+       //Console.WriteLine(col[position] );
+        if (col[position] > 0)
+        {
+          chislb0++;
+          position++;
+        }
+        else
+       position++; 
+    }
+    Console.WriteLine($"чисел положительных всего - {chislb0} ");
+    //Console.WriteLine();
+}
+Console.WriteLine("сколько чисел в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводите числа массива");
+int[] array = new int [n];
+Fillarray(array);
+PrintArray(array);
---
+++ Домашка6/Задание2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите  b k для первой точки");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите  b k для второй точки");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
