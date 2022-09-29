8f41c1e2b42e6743b0466ed670dc9d5b00a83b95
Nameuser9 <ivamand2002@gmail.com>
2022-09-30 00:21:21 +0700

    Сделал последнее задание
    Задача с трехмерным массивом так и не далась(((

---
+++ ПОСЛЕДНЯЯ ДОМАШКА/Задача2.cs
@@ -0,0 +1,69 @@
+﻿void Fillarray2(int[,] collection)
+{
+    
+    //int length = collection.Length;
+    //int index = 0;
+    for (int chet = 0; chet < collection.GetLength(0); chet++)
+    {
+        for (int chet2 = 0; chet2 < collection.GetLength(1); chet2++)
+        collection[chet,chet2] =new Random().Next(1, 20);                    //;Convert.ToInt32(Console.ReadLine())
+    }
+}
+void PrintArray2(int[,] col)
+{
+    //int count = col.Length;
+    //int position = 0;
+   for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+        Console.Write($"{ col [i , j]} ");        
+        }
+    Console.WriteLine();
+    }  
+}
+Console.WriteLine("сколько должно быть в массве 1:строк, 2 столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m,n];
+Console.WriteLine();
+Fillarray2(array);
+PrintArray2(array);
+Console.WriteLine(); //Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//int[] sumarray = new int [m];//массив с суммами строк двумерного массива
+int a = 0;
+int sum = 0;
+int SMPNS = 0;//СамаяМаленькаяПеременнаяНаСвете
+int i = 0;
+while (a < m)
+{
+sum = 0;
+int b = 0;
+
+    while (b < n)
+    {
+        sum +=  array[a,b];
+        b++; 
+        //sumarray[a] = sum;
+    }
+    if (a < 1)
+        if (SMPNS >= sum)
+        {}
+        else
+         {
+         SMPNS = sum; 
+         }
+        
+    if (a > 0)
+        if (SMPNS >= sum)
+        {
+            SMPNS = sum;
+            i = a;
+        }
+a++;
+    //Console.Write($" { sum} ");
+}
+Console.WriteLine();
+//Console.Write($" { SMPNS} ");
+Console.WriteLine(i);
---
+++ ПОСЛЕДНЯЯ ДОМАШКА/Задача3.cs
@@ -0,0 +1,54 @@
+﻿void Fillarray2(int[,] collection)
+{
+    
+    //int length = collection.Length;
+    //int index = 0;
+    for (int chet = 0; chet < collection.GetLength(0); chet++)
+    {
+        for (int chet2 = 0; chet2 < collection.GetLength(1); chet2++)
+        collection[chet,chet2] =new Random().Next(-1000, 1001);                    //;Convert.ToInt32(Console.ReadLine())
+    }
+}
+void PrintArray2(int[,] col)
+{
+    //int count = col.Length;
+    //int position = 0;
+   for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+        Console.Write($"{ col [i , j]} ");        
+        }
+    Console.WriteLine();
+    }  
+}
+Console.WriteLine("сколько должно быть в массве 1:строк, 2 столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m,n];
+int[,] array2 = new int [m,n];
+int[,] resultarray = new int [m,n];
+Fillarray2(array);
+PrintArray2(array);
+Console.WriteLine();
+Fillarray2(array2);
+PrintArray2(array2);
+
+if (array.GetLength(0) != array2.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        resultarray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultarray[i, j] += array[i, k] * array2[k, j];
+        }
+    }
+}
+Console.WriteLine();
+PrintArray2(resultarray);
