de39b36dd638dd12e2a28983a13848eb71fe0f66
Nameuser9 <ivamand2002@gmail.com>
2022-10-03 23:34:52 +0700

    Сделал последний проект

---
+++ ПОСЛЕДНЯЯ ПОСЛЕДНЯЯ ДОМАШКА/Задание1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите числа сумму чисел между которыми хотите видеть");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int vyvod = 0;
+while (n <= m)
+{
+vyvod += n;
+n++;
+}
+Console.WriteLine(vyvod);
